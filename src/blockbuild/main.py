1d08488fbf119fac9ca3c54651a2b8db9abdc01b
Boryslav Larin <brabadu@gmail.com>
2014-10-29 23:29:14 +0200

    main.py cleanup

--- src/blockbuild/main.py
+++ src/blockbuild/main.py
@@ -2,31 +2,21 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
-# configuration
-DATABASE = '/tmp/flaskr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
-
-# create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-# app.config.from_envvar('BLOCKBUILD_SETTINGS', silent=True)
-
-# def connect_db():
-#     return sqlite3.connect(app.config['DATABASE'])
 
 from blocks import container_node
 from pml.render import render_pml_template
 
-TEMPLATE = """<container>
+
+TEMPLATE = """
+<container>
     <header />
     <sub_a />
     <editor />
-</container>""".strip()
+</container>"""
 
 
 @app.route('/', methods=['GET', 'POST'])
